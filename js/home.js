fa4b166961c6c9ceecd1e8e4fea277c8597bb8ff
jeilers96 <jeilers96@gmail.com>
2018-02-02 20:28:06 -0600

    make about page responsive and filter menu

--- js/home.js
+++ js/home.js
@@ -1,22 +1,22 @@
 function filterSelection(category){
-  var tiles, i;
-  tiles = document.getElementsByClassName("tile"); //get all tiles
-  if (category == "All"){
-	category = ""; //if all is selected then see all tiles
-  } 
-  for (i = 0; i < tiles.length; i++){
-	removeClass(tiles[i], "show");
-	if (tiles[i].className.indexOf(category) > -1){ 
-		addClass(tiles[i], "show");
+	var tiles, i;
+	tiles = document.getElementsByClassName("tile"); //get all tiles
+	if (category == "All"){
+		category = ""; //if all is selected then see all tiles
 	} 
-  }
+	for (i = 0; i < tiles.length; i++){
+		removeClass(tiles[i], "show");
+		if (tiles[i].className.indexOf(category) > -1){ 
+			addClass(tiles[i], "show");
+		} 
+	}
 }
 
 function setActiveSelection(){
 	var catSelectors, i;
 	catSelectors = document.getElementsByClassName("catSelector");
 	for (i = 0; i < catSelectors.length; i++){
-	  catSelectors[i].addEventListener("click", function(){
+		catSelectors[i].addEventListener("click", function(){
 		document.getElementById("activeCat").removeAttribute("id");
 		this.id += "activeCat";
 	  });
@@ -24,28 +24,37 @@ function setActiveSelection(){
 
 }
 function addClass(element, name){
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-	if (arr1.indexOf(arr2[i]) == -1) {
-		element.className += " " + arr2[i];
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		if (arr1.indexOf(arr2[i]) == -1) {
+			element.className += " " + arr2[i];
+		}
 	}
-  }
 }
 
 function removeClass(element, name){
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-	while (arr1.indexOf(arr2[i]) > -1) {
-	  arr1.splice(arr1.indexOf(arr2[i]), 1);     
-	}
-  }
-  element.className = arr1.join(" ");
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		while (arr1.indexOf(arr2[i]) > -1) {
+			arr1.splice(arr1.indexOf(arr2[i]), 1);     
+		}
+	}	
+	element.className = arr1.join(" ");
 }
 
+function dropDown() {
+	var x = document.getElementById("filterNav");
+	if (x.className === "filter") {
+		x.className += " responsive";
+	} else {
+		x.className = "filter";
+	}
+}
+			
 $(function(){
 		var includes = $('[data-include]');
 		jQuery.each(includes, function(){
